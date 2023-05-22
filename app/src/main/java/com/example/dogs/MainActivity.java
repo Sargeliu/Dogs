ebe56f8961b0ce96dc263a9019d4e56de8ea1270
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2023-05-22 10:29:52 +0500

    Работа с интернетом и StringBuilder

--- app/src/main/java/com/example/dogs/MainActivity.java
+++ app/src/main/java/com/example/dogs/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -16,13 +20,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadDogImage();
     }
 
     private void loadDogImage() {
-        try {
-            URL url = new URL(BASE_URL);
-        } catch (Exception e) {
-            Log.d("MainActivity", e.getMessage());
-        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(BASE_URL);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream inputStream = urlConnection.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                    StringBuilder data = new StringBuilder();
+                    String result;
+                    do {
+                        result = bufferedReader.readLine();
+                        if (result != null) {
+                            data.append(result);
+                        }
+                    } while (result != null);
+                    Log.d("LogActivity", data.toString());
+                } catch (Exception e) {
+                    Log.d("LogActivity", e.toString());
+                }
+            }
+        }).start();
     }
 }
